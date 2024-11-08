b7456a968b6db49e85637a7fe37c353a56114380
nando <hdeza989@gmail.com>
2024-11-08 03:15:16 -0500

    Animacion añadida: Se añadió una animación para que se mueva la pantalla hacia el contenido generado por la IA junto con un defereable view

--- src/app/shared/components/main-prediction/main-prediction.component.ts
+++ src/app/shared/components/main-prediction/main-prediction.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { ItineraryService } from '../../../core/services/itinerary.service';
 import { ItineraryInfo } from '../../../core/models/itinerary.model';
 import { WeatherService } from '../../../core/services/weather.service';
 import { ClimaFormatter, Coordenadas } from '../../../core/models/clima.model';
 import { TemperatureService } from '../../../core/services/temperature.service';
 import { lastValueFrom } from 'rxjs';
+import { WeatherComponent } from '../../../features/weather/weather.component';
+import { ItineraryComponent } from '../../../features/itinerary/itinerary.component';
 
 @Component({
   selector: 'app-main-prediction',
   standalone: true,
-  imports: [],
+  imports: [WeatherComponent, ItineraryComponent],
   templateUrl: './main-prediction.component.html',
   styleUrl: './main-prediction.component.scss',
 })
 export class MainPredictionComponent {
   backGroundUrl: string = 'images/travel.jpg';
   city: string = '';
-  data: any;
+  dataItinerary: any;
   weatherData: any;
+  showData: boolean = false;
+
+  @ViewChild('dataSection') dataSection!: ElementRef; // Referencia al contenedor de datos
 
   coordinateCitySelected: Coordenadas = {
     lat: 0,
@@ -66,12 +71,16 @@ export class MainPredictionComponent {
     .padStart(2, '0')}-${this.today.getDate().toString().padStart(2, '0')}`;
 
   // Función que esta pendiente de cualquier cambio en el select y guarda el valor
-  onChange(event: Event) {
+  onChangeCity(event: Event) {
     const city = (event.target as HTMLSelectElement).value;
     this.city = city;
     this.dataInfo.city = this.city;
   }
 
+  onChangeDays(event: Event) {
+    const days = (event.target as HTMLSelectElement).value;
+    this.dataInfo.days = parseInt(days);
+  }
   /*-
     Uso de lastValueFrom: El operador lastValueFrom convierte el observable en una promesa, 
     permitiendo usar await en la llamada a getWeather y getTemperature.
@@ -107,20 +116,30 @@ export class MainPredictionComponent {
     const dataT = await lastValueFrom(
       this.temperatureService.postTemperature(this.paramsWeather)
     );
-    this.dataInfo.temperature = dataT[0];
+    this.dataInfo.temperature = dataT[0].toFixed(1);
 
     console.log('Datos de la temperatura');
     console.log(this.dataInfo.temperature);
   }
 
   async getItinerary(): Promise<void> {
+    console.log('hola desde getItinerary');
     const dataI = await lastValueFrom(
       this.itineraryService.postData(this.dataInfo)
     );
 
-    this.data = dataI;
+    this.dataItinerary = dataI;
     console.log('Datos del itinerario');
-    console.log(this.data);
+    console.log(this.dataItinerary);
+    this.showData = true;
+
+    // Desplazamiento suave hacia el componente
+    setTimeout(() => {
+      this.dataSection.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }, 0);
   }
 
   async createTrip(): Promise<void> {
